60b0e4df13be5c45696a6c02b88cbf10c31c5861
Minsu Kim <alstn2468_@naver.com>
2022-02-05 20:24:19 +0900

    [alstn2468] feat: Proxy 패턴 실습 추가

---
+++ alstn2468/2_proxy/practice/src/index.ts
@@ -0,0 +1,67 @@
+type Form = {
+  name: string,
+  age: number;
+}
+type Prop = keyof Form;
+type Value = Form[Prop];
+
+let validName: boolean | undefined,
+  validAge: boolean | undefined;
+const form: Form = {
+  name: '',
+  age: 0,
+};
+
+const nameValidator = (name: Form['name']) => name !== '' && name.length <= 4;
+const ageValidator = (age: Form['age']) => age > 0 && age <= 4;
+const validateObject = (prop: Prop, value: Value) => {
+  switch (prop) {
+    case 'age':
+      if (ageValidator(Number(value))) {
+        validAge = true;
+      } else {
+        validAge = false;
+      }
+      return validAge;
+    case 'name':
+      if (typeof value === 'string' && nameValidator(value)) {
+        validName = true;
+      } else {
+        validName = false;
+      }
+      return validName;
+  }
+}
+
+const formProxy = new Proxy(form, {
+  get: (obj: Form, prop: Prop) => {
+    return Reflect.get(obj, prop);
+  },
+  set: (obj: Form, prop: Prop, value: Value) => {
+    if (validateObject(prop, value)) {
+      return Reflect.set(obj, prop, value);
+    }
+    return false;
+  }
+});
+
+const nameInput = document.querySelector('#name');
+const ageInput = document.querySelector('#age');
+
+nameInput.addEventListener('input', (e) => {
+  formProxy.name = (<HTMLInputElement>e.target).value;
+  if (!validName) {
+    nameInput.setAttribute('class', 'error');
+  } else {
+    nameInput.setAttribute('class', 'valid');
+  }
+});
+
+ageInput.addEventListener('keyup', (e) => {
+  formProxy.age = (<HTMLInputElement>e.target).value;
+  if (!validAge) {
+    ageInput.setAttribute('class', 'error');
+  } else {
+    ageInput.setAttribute('class', 'valid');
+  }
+});
